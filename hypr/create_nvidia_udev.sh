50cded73734b034d958ea74634dcc7036f2ef9da
Daniel Gafarov <gafarovdaniel16@gmail.com>
2026-09-05 12:46:25 +0200

    first commit

---
+++ hypr/create_nvidia_udev.sh
@@ -0,0 +1,21 @@
+echo "blacklist nouveau
+options nouveau modeset=0" > /etc/modprobe.d/blacklist-nouveau.conf
+
+echo '# Remove NVIDIA USB xHCI Host Controller devices, if present
+ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
+
+# Remove NVIDIA USB Type-C UCSI devices, if present
+ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
+
+# Remove NVIDIA Audio devices, if present
+ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
+
+# Remove NVIDIA VGA/3D controller devices
+ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"' > /etc/udev/rules.d/00-remove-nvidia.rules
+
+powerprofilesctl set power-saver
+
+sed -i 's/    disabled = true/    disabled = false/' /home/daniel/.config/hypr/hyprland.lua
+
+reboot
+
